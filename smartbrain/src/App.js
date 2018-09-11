0e07a8405e2645ecafb0254aab598b662420efe6
Kin L Lim <Lim.Kin@gmail.com>
2018-09-11 16:39:58 -0400

    Connected Backend to Frontend

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import Navigation from "./Components/Navigation/Navigation";
 import Signin from "./Components/Signin/Signin";
 import Register from "./Components/Register/Register";
-import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from "./Components/Rank/Rank";
 import "./App.css";
 
-// const API_KEY = process.env.API_KEY;
-
+//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
   apiKey: ""
 });
@@ -19,7 +18,7 @@ const app = new Clarifai.App({
 const particlesOptions = {
   particles: {
     number: {
-      value: 100,
+      value: 30,
       density: {
         enable: true,
         value_area: 800
@@ -36,9 +35,28 @@ class App extends Component {
       imageUrl: "",
       box: {},
       route: "signin",
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: ""
+      }
     };
   }
+
+  loadUser = data => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    });
+  };
 
   calculateFaceLocation = data => {
     const clarifaiFace =
@@ -49,16 +67,16 @@ class App extends Component {
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
     };
   };
 
-  displayFaceBox = (box) => {
+  displayFaceBox = box => {
     this.setState({ box: box });
   };
 
-  onInputChange = (event) => {
+  onInputChange = event => {
     this.setState({ input: event.target.value });
   };
 
@@ -66,44 +84,66 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then(response => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch(err => console.log(err));
   };
 
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      this.setState({ isSignedIn: false })
-    } else if (route === 'home ') {
-      this.setState({ isSignedIn: true })
+  onRouteChange = route => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route})
-  }
+    this.setState({ route: route });
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
-
-    return <div className="App">
+    return (
+      <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === "home" 
-          ? <div>
-              <Logo />
-              <Rank />
-              <ImageLinkForm 
-                onInputChange={this.onInputChange} 
-                onButtonSubmit={this.onButtonSubmit} 
-              />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-          </div> 
-          : (
-              route === "signin" 
-              ? <Signin onRouteChange={this.onRouteChange} /> 
-              : <Register onRouteChange={this.onRouteChange} />
-            )
-          }
-      </div>;
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
+    );
   }
 }
 
--- smartbrain/src/Components/Navigation/Navigation.js
+++ smartbrain/src/Components/Navigation/Navigation.js
@@ -6,7 +6,8 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
       <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p
           onClick={() => onRouteChange("signout")}
-          className="f3 link dim black underline pa3 pointer">
+          className="f3 link dim black underline pa3 pointer"
+        >
           Sign Out
         </p>
       </nav>
@@ -16,12 +17,14 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
       <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p
           onClick={() => onRouteChange("signin")}
-          className="f3 link dim black underline pa3 pointer">
+          className="f3 link dim black underline pa3 pointer"
+        >
           Sign In
         </p>
         <p
           onClick={() => onRouteChange("register")}
-          className="f3 link dim black underline pa3 pointer">
+          className="f3 link dim black underline pa3 pointer"
+        >
           Register
         </p>
       </nav>
